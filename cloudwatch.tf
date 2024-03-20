f31f69c49222cfdc4fe24e470e17f844c2a42f12
Patomon002 <patrickomorovan@gmail.com>
2024-03-20 16:48:08 +0000

    upddate to cloudwatch

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,4 +1,24 @@
-module "cloudwatch" {
-  source  = "terraform-aws-modules/cloudwatch/aws"
-  version = "5.3.1"
+ #Metrics for memory and disk utilization
+ #This metrics should be stored on AWS System manager - SSM
+
+ {
+	"metrics": {
+		"append_dimensions": {
+			"InstanceId": "${aws:InstanceId}"
+		},
+		"metrics_collected": {
+			"mem": {
+				"measurement": [
+					"mem_used_percent"
+				],
+				"metrics_collection_interval": 60
+			},
+            "disk": {
+				"measurement": [
+                     "disk_used_percent"
+				],
+				"metrics_collection_interval": 60
+			}
+		}
+	}
 }
