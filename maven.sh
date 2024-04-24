521877b38f9094c4e461e4b23befb02014c38a64
Patomon002 <patrickomorovan@gmail.com>
2024-04-24 17:00:19 +0100

    add

--- maven.sh
+++ maven.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
-sudo yum update
-sudo yum upgrade -y
+# sudo yum update
+# sudo yum upgrade -y
 
-sudo wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+# sudo wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 
-sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+# sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 
-sudo yum get-update && sudo yum install -y collectd
+# sudo yum get-update && sudo yum install -y collectd
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:/alarm/AWS-CWAgentLinConfig -s
+# sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:/alarm/AWS-CWAgentLinConfig -s
 
-sudo hostnamectl set-hostname maven
-
+# sudo hostnamectl set-hostname maven
+ 
 cd /opt
 
-sudo yum install wget nano tree unzip git-all -y
+sudo mkdir maven
+
+sudo yum install wget tar gzip tree unzip git-all -y
 
 sudo yum install java-11-openjdk-devel java-1.8.0-openjdk-devel -y
 
-sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.zip
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
 
+sudo tar xvf apache-maven-3.9.6-bin.tar.gz
 
-sudo unzip apache-maven-3.9.6-bin.zip
+sudo rm -rf apache-maven-3.9.6-bin.tar.gz
 
-sudo rm -rf apache-maven-3.9.6-bin.zip
+sudo mv apache-maven-3.9.6 maven
 
-sudo mv apache-maven-3.9.6/ maven
+sudo export M2_HOME=/opt/maven/apache-maven-3.9.6
 
-sudo echo export M2_HOME=/opt/maven >> ~/.bash_profile 
+sudo export M2=$M2_HOME/bin
 
-sudo echo export PATH=$PATH:$M2_HOME/bin >> ~/.bash_profile 
+sudo export PATH=$M2:$PATH
